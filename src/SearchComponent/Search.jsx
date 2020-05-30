0d24b10ca075a783143a03ad29c81ef5f3117801
Akash Kumar <Akash.dhaliya@gmail.com>
2020-05-30 12:45:24 +0530

    Latest Code

--- src/SearchComponent/Search.jsx
+++ src/SearchComponent/Search.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React from "react";
+import { FiSearch } from "react-icons/fi";
 
-import './Search.css';
+import "./Search.css";
 
 class SearchComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchValue: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: "",
+    };
+  }
 
-    render() {
-        return (
-            <section>
-                <input className="Search-Query" type="text" placeholder="Search photos here.." name="search" />
-            </section>
-            
-        );
+  searchInputChange = (event) => {
+    let regex = /^[a-zA-Z]*$/;
+    if (regex.test(event.target.value)) {
+      return true;
+    } else {
+      return false;
     }
+  };
+  render() {
+    return (
+      <section>
+        <input
+          onInput={this.searchInputChange}
+          className="Search-Query"
+          type="text"
+          placeholder="Search photos here.."
+          name="search"
+        />
+        <span className="search-icon-span">
+          <FiSearch />
+        </span>
+      </section>
+    );
+  }
 }
 
 export default SearchComponent;
