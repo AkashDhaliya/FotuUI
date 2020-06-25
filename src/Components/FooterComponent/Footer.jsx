f3bef25f7dc29dfd525ffed0b649e7a542efc166
Akash Kumar <Akash.dhaliya@gmail.com>
2020-06-25 12:25:56 +0530

    Fixed CSS

---
+++ src/Components/FooterComponent/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <div>
+        <p>
+          {" "}
+          Created by Akash. Contact me at{" "}
+          <strong>Akash.dhaliya@gmail.com</strong>
+        </p>
+        <p>
+          {" "}
+          Images source <a href="https://unsplash.com/">Unsplash&nbsp;</a>Code
+          source <a href="https://github.com/AkashDhaliya/Fotu.git">Github</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
